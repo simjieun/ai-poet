547349032c44759e586ab3b2f78d162b062b498c
simjieun <simjieun@encar.com>
2024-07-26 10:06:00 +0900

    dotenv 주석처리

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from dotenv import load_dotenv
-load_dotenv()
+# from dotenv import load_dotenv
+# load_dotenv()
 
 from langchain_openai import ChatOpenAI
 from langchain_core.messages import HumanMessage, SystemMessage
